d2ea7ea19ed8d04cef216c02b5c4f9e34a2caaae
aleks gil carvalho <aleksgillacerda@gmail.com>
2025-01-26 03:31:28 +0000

    exercicio final de dicionario

--- lista_dicionarios.py
+++ lista_dicionarios.py
@@ -1,5 +1,11 @@
+pet_1 = {'animal':'cachorro', 'idade':4, 'barulho':'latido'}
+pet_2 = {'animal':'gato', 'idade':6, 'barulho':'miado'}
+pet_3 = {'animal':'priquito', 'idade':6, 'barulho':'piu'}
 
-favorite_languages = {'jen': ['python', 'ruby'], 'sarah': ['c'], 'edward': ['ruby', 'go'],
-'phil': ['python', 'haskell'], }
+pets = [pet_1, pet_2, pet_3]
 
-print(favorite_languages['jen'][1])
+for pet in pets:
+    print('')
+    for key, value in pet.items():
+        if value == 'cachorro':
+            print(key, value)
