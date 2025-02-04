b45569bf91b0dcbfdb0e8fe5520b2175c643ad42
aleks gil carvalho <aleksgillacerda@gmail.com>
2025-02-04 02:11:25 +0000

    agora  usando o pop

--- while.py
+++ while.py
@@ -1,12 +1,14 @@
 sandwich_orders = ['pastrami' ,'misto quente', 'pastrami' , 'podrão', 'artesanal', 'pastrami' ]
-finished_sandwiches = []
 new_sanduiches = []  #o exerxício pediu ra usar o laço while, mas não precisava, agora vou tentar fazer com o while
 
 print('\n Não temos pastrami.')
 
-while 'pastrami'in sandwich_orders:
-       sandwich_orders.remove('pastrami')
+while sandwich_orders:
+   sanduiche = sandwich_orders.pop()
+   if sanduiche != 'pastrami':
+       new_sanduiches.append(sanduiche)
 
-for sanduiche in sandwich_orders:
+
+for sanduiche in new_sanduiches:
     print(f'O seu {sanduiche} está pronto')
 
