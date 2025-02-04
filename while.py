168e6b8e980b0e5ee8070b1e975885e2eb30a15e
aleks gil carvalho <aleksgillacerda@gmail.com>
2025-02-04 00:55:50 +0000

    tirando o pastrami

--- while.py
+++ while.py
@@ -1,7 +1,14 @@
 sandwich_orders = ['pastrami' ,'misto quente', 'pastrami' , 'podrão', 'artesanal', 'pastrami' ]
 finished_sandwiches = []
+new_sanduiches = []
 print('\n No momento estamos sem pastrami \n' )
 for sanduiche in sandwich_orders:
+    if sanduiche != 'pastrami':
+        new_sanduiches.append(sanduiche)
+    sanduiches_de_hoje = ', '.join(new_sanduiches)
+print(f'os sanduiches de hoje são {sanduiches_de_hoje}')
+
+for sanduiche in new_sanduiches:
     print(f' preparei o seu {sanduiche}')
     finished_sandwiches.append(sanduiche)
 print()
