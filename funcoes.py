0f8552098b394485158d0dabe3089f6c08f8466a
aleks gil carvalho <aleksgillacerda@gmail.com>
2025-02-11 02:27:21 +0000

    valores de retorno

--- funcoes.py
+++ funcoes.py
@@ -1,5 +1,6 @@
-def describe_pet(pet_name, animal_type='dog'): 
-    """Exibe informaçõessobre um animal de estimação."""
-    print("\nI have a " + animal_type + ".") 
-    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
-describe_pet(pet_name='willie')
+def get_formatted_name(first_name, last_name):
+    """Devolve um nome completo formatado de modo elegante."""
+    full_name = first_name + ' ' + last_name 
+    return full_name.title()
+musician = get_formatted_name('jimi', 'hendrix') 
+print(musician)
