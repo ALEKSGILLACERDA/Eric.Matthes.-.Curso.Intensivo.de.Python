c0cfef3e084e859808cda4f5db1213aa32c69a8f
aleks gil carvalho <aleksgillacerda@gmail.com>
2025-02-11 02:08:13 +0000

    colocando nomes, valores mos  parametros

--- funcoes.py
+++ funcoes.py
@@ -1,8 +1,5 @@
-
-
-def describe_pet(animal_type, pet_name): 
+def describe_pet(animal_type, pet_name):
     """Exibe informações sobre um animal de estimação."""
-
-    print("\nI have a " + animal_type + ".")
-    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
-describe_pet('hamster', 'harry')
+    print("\nI have a " + animal_type + ".") 
+    print("My " + animal_type +"'s name is " + pet_name.title() + ".")
+describe_pet(animal_type='hamster', pet_name='harry')
