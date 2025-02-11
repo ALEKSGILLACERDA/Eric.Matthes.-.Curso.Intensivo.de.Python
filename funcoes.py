8403c083a260dd456ea00b8ab14361ab6b5a73f0
aleks gil carvalho <aleksgillacerda@gmail.com>
2025-02-11 02:20:19 +0000

    valores default

--- funcoes.py
+++ funcoes.py
@@ -1,5 +1,5 @@
-def describe_pet(animal_type, pet_name):
-    """Exibe informações sobre um animal de estimação."""
+def describe_pet(pet_name, animal_type='dog'): 
+    """Exibe informaçõessobre um animal de estimação."""
     print("\nI have a " + animal_type + ".") 
-    print("My " + animal_type +"'s name is " + pet_name.title() + ".")
-describe_pet(animal_type='hamster', pet_name='harry')
+    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+describe_pet(pet_name='willie')
