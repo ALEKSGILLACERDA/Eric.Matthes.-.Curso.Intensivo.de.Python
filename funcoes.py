5c01cf8e574ed1121a7d036d79c5ab6b6574ba5d
aleks gil carvalho <aleksgillacerda@gmail.com>
2025-02-25 04:20:41 +0000

    tranferindo de uma lista para outra, sem funcao

--- funcoes.py
+++ funcoes.py
@@ -1,11 +1,8 @@
-usernames =  ['hannah', 'ty', 'margot']
-
-def greet_users(names):
-  for name in names:
-    msg = f'Hello {name.title()}'
-    print(msg)
-#passamos um parametro em greet user, e usamos a lista como argumento, ficou um pouco confuso
-#mas lembra que podemos colocar qualquer variável na função? e depois chamar qualquer coisa
-#chamamos isso de argumento posicional, no caso names aí se torna a lista usernames
-#quando a gente chama a função com a lista, complicado, mas funciona
-greet_users(usernames)
+unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+current_design = unprinted_designs.pop()
+completed_models.append(current_design)
+print("Printing model: " + current_design)
+print("\nThe following models have been printed:")
+for completed_model in completed_models:
+    print(completed_model)
