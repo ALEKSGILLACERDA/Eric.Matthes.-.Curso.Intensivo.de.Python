7fcdd9ceca5f0cd0b7445713ad4fc35e9b170969
aleks gil carvalho <aleksgillacerda@gmail.com>
2025-02-25 03:54:43 +0000

    chamando uma funcao com uma lista

--- funcoes.py
+++ funcoes.py
@@ -1,6 +1,11 @@
-def get_formatted_name(first_name, last_name):
-    """Devolve um nome completo formatado de modo elegante."""
-    full_name = first_name + ' ' + last_name 
-    return full_name.title()
-musician = get_formatted_name('jimi', 'hendrix') 
-print(musician)
+usernames =  ['hannah', 'ty', 'margot']
+
+def greet_users(names):
+  for name in names:
+    msg = f'Hello {name.title()}'
+    print(msg)
+#passamos um parametro em greet user, e usamos a lista como argumento, ficou um pouco confuso
+#mas lembra que podemos colocar qualquer variável na função? e depois chamar qualquer coisa
+#chamamos isso de argumento posicional, no caso names aí se torna a lista usernames
+#quando a gente chama a função com a lista, complicado, mas funciona
+greet_users(usernames)
